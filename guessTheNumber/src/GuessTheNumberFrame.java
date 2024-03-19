75fe20208e6d070cb9d2af205d6017a80988524d
DovgaNik <nikita.dovgan.05@hotmail.com>
2024-03-19 12:15:14 +0200

    Added elements to JFrame

--- guessTheNumber/src/GuessTheNumberFrame.java
+++ guessTheNumber/src/GuessTheNumberFrame.java
@@ -1,14 +1,30 @@
 import javax.swing.*;
 import java.awt.*;
+import java.text.MessageFormat;
 
 public class GuessTheNumberFrame extends JFrame{
-    JLabel instruction, afterInput, livesLeft;
+    JLabel instructions, afterInput, livesLeft;
     JButton restart;
     JTextField numberInput;
+    private int MAX_VALUE = 20,
+        lives = 5;
     public GuessTheNumberFrame(){
         super("Guess the Number");
-        setSize(300, 500);
+        setSize(300, 300);
         setLayout(new GridLayout(5, 1));
+
+        instructions =  new JLabel(MessageFormat.format("Guess a number between 1 and {0}", MAX_VALUE));
+        afterInput = new JLabel("Hello, welcome to our wonderful game🎮🕹️");
+        livesLeft = new JLabel(MessageFormat.format("You have {0} lives left", lives));
+        numberInput = new JTextField();
+        restart = new JButton("Restart");
+
+        add(instructions);
+        add(numberInput);
+        add(afterInput);
+        add(livesLeft);
+        add(restart);
+
         setVisible(true);
     }
 }
